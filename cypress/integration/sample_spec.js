cb51bae0f9907120396a741b704b9ffead79c3c9
Teemu Pärssinen <teemu.parssinen@wunder.io>
2021-05-19 14:10:52 +0300

    edit the sample site and the test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,7 @@
 describe('My First Test', () => {
   it('Visits the test site', () => {
     cy.visit('http://localhost:3000')
+
+    cy.contains('Click me!').click()
   })
 })
--- test-site/pages/index.js
+++ test-site/pages/index.js
@@ -1,63 +1,32 @@
 import Head from 'next/head'
 
 export default function Home() {
+  let text = "Click me!";
+
+  const handleClick = e => {
+    e.preventDefault();
+    const el = document.getElementsByClassName("action")[0];
+    el.text = "Clicked!"
+  };
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Test App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
+            href="/"
+            className="card action"
+            onClick={handleClick}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            {text}
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -158,6 +127,9 @@ export default function Home() {
           border: 1px solid #eaeaea;
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
+          text-align: center;
+          text-transform: uppercase;
+          font-weight: bold;
         }
 
         .card:hover,
