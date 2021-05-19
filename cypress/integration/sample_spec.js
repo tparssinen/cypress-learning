27e06332b910c10de3b1a39816634e4b7012ec44
Teemu Pärssinen <teemu.parssinen@wunder.io>
2021-05-19 13:10:06 +0300

    change sample test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+  it('Visits the test site', () => {
+    cy.visit('http://localhost:3000')
   })
 })
